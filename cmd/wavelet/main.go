3e77bf99e83d458aee92d97b21e52e08b1808306
Vincent Garrigues <vincent@soundcloud.com>
2018-05-02 20:18:14 +0200

    Badly render points

--- cmd/wavelet/main.go
+++ cmd/wavelet/main.go
@@ -6,6 +6,10 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/soundcloud/wavelet/lib/sampler"
+	"github.com/soundcloud/wavelet/lib/wav"
 )
 
 func main() {
@@ -18,5 +22,23 @@ func main() {
 }
 
 func ok(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "ok!")
+	file, err := os.Open("fixtures/fixture.wav")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	samples, err := wav.Samples(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	points, err := sampler.Sample(samples, 1200)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	io.WriteString(w, fmt.Sprintf("%v", points))
 }
