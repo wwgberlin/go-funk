de43514b60cdff29dc2d9952e48555fae4a2e92e
Vincent Garrigues <vincent@soundcloud.com>
2018-05-03 07:56:21 +0200

    Add /points endpoint

--- cmd/wavelet/main.go
+++ cmd/wavelet/main.go
@@ -3,13 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/soundcloud/wavelet/lib/sampler"
-	"github.com/soundcloud/wavelet/lib/wav"
 )
 
 func main() {
@@ -17,28 +12,11 @@ func main() {
 	host := fmt.Sprintf(":%s", *port)
 
 	http.HandleFunc("/", ok)
+	http.HandleFunc("/points", points)
 
 	log.Fatal(http.ListenAndServe(host, nil))
 }
 
 func ok(w http.ResponseWriter, req *http.Request) {
-	file, err := os.Open("fixtures/fixture.wav")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	samples, err := wav.Samples(file)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	points, err := sampler.Sample(samples, 1200)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	io.WriteString(w, fmt.Sprintf("%v", points))
+	fmt.Fprintln(w, "ok")
 }
