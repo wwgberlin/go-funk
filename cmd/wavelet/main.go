4db0c297bc881929659a2c9f7d0dc4d5c191af70
Vincent Garrigues <vincent@soundcloud.com>
2018-05-07 09:09:50 +0200

    Use net.JoinHostPort

--- cmd/wavelet/main.go
+++ cmd/wavelet/main.go
@@ -4,12 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
 	var port = flag.String("port", "8080", "server port.")
-	host := fmt.Sprintf(":%s", *port)
+	addr := net.JoinHostPort("", *port)
 
 	http.HandleFunc("/", ok)
 	http.HandleFunc("/points", points)
@@ -17,7 +18,7 @@ func main() {
 	http.HandleFunc("/gif", gifHandler)
 	http.HandleFunc("/gif2", gifHandler2)
 
-	log.Fatal(http.ListenAndServe(host, nil))
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func ok(w http.ResponseWriter, req *http.Request) {
