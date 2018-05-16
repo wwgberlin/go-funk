52473c2405f4b802af1b3d0991eeaaee6fea5c90
Vincent Garrigues <vincent@soundcloud.com>
2018-05-16 20:35:05 +0200

    AvgAbs

--- sampler/avg_abs.go
+++ sampler/avg_abs.go
@@ -3,5 +3,9 @@ package sampler
 // AvgAbs calculates the absolute value of the average
 // of the values in the given slice
 func AvgAbs(in []int) int {
-	return 0
+	acc := 0
+	for _, v := range in {
+		acc += v
+	}
+	return acc / len(in)
 }
