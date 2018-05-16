b9b57d24c86d27beede8b121a96a320fc5bf9177
Vincent Garrigues <vincent@soundcloud.com>
2018-05-16 20:06:41 +0200

    AbsAvg

--- sampler/abs_avg.go
+++ sampler/abs_avg.go
@@ -2,5 +2,16 @@ package sampler
 
 // AbsAvg calculates the average of the absolute values in the given slice
 func AbsAvg(in []int) int {
-	return 0
+	acc := 0
+	for _, v := range in {
+		acc += abs(v)
+	}
+	return acc / len(in)
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
 }
